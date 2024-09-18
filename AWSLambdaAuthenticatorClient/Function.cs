d49f814f0f2953949dec8fe09da32f6e59b5159a
ricardosn87 <49949770+ricardosn87@users.noreply.github.com>
2024-09-18 20:37:44 -0300

    Update Function.cs

--- AWSLambdaAuthenticatorClient/Function.cs
+++ AWSLambdaAuthenticatorClient/Function.cs
@@ -36,7 +36,7 @@ public class Function
             DataNascimento = "28/06/1988",
             Email = "ricardosn87@hotmail.com",
             Id = 19,
-            Nome = "Ricardo Nogueira teste action 2",
+            Nome = "Ricardo Nogueira teste action 3",
             Texto = body
         };
         return new APIGatewayHttpApiV2ProxyResponse
